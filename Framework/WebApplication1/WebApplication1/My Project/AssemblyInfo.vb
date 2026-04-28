ff1848d7a206d40ba45428c99e60ec8420a0cc28
Neon <wiseearl@gmail.com>
2026-04-28 15:33:48 +0800

    1.0 init

---
+++ Framework/WebApplication1/WebApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 組件的一般資訊是由下列的屬性集 
+' 控制。變更這些屬性值可修改與組件關聯的
+' 資訊。
+
+' 檢閱組件屬性的值
+<Assembly: AssemblyTitle("WebApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication1")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+<Assembly: Guid("1ac5d19f-142a-4993-9961-8fa78e9dbb39")>
+
+' 組件的版本資訊是由下列四項值構成:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
